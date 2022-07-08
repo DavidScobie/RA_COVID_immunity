2bbeebf6eb78f4e0cd1029c8967fa99e94636a31
David Scobie <david.scobie.20@ucl.ac.uk>
2022-07-08 16:07:16 +0100

    bound data frames together for patients

--- TCR_analysis/all_patients_plot_together.R
+++ TCR_analysis/all_patients_plot_together.R
@@ -15,42 +15,29 @@ library(tidyr)
 library(pracma)
 library(MASS)
 
-#read in the 3 timepoints for 1st patient alpha chain
+##To start with we want to look at all PCR+ve patients from day 0 to day 7, on both alpha and beta chains
+
+#read in the data for all of the patients with no filename, on days 0, 7 and 14, with only necessary data inside
 data_path<-"C:/Research_Assistant/work/data/TCR_data/"
 input_data_chop_all<-read.csv(paste0(data_path,"input_data_chop_all_reg_days_no_filename.csv"))
 
-# #exclude all rows which contain day10, day13 and post as these make data frame too big
-# input_data_chop_all_without_10 <- input_data_chop_all[!grepl("day10", input_data_chop_all$filename),]
-# input_data_chop_all_without_10_13 <- input_data_chop_all_without_10[!grepl("day13", input_data_chop_all_without_10$filename),]
-# input_data_chop_all_without_reg_days <- input_data_chop_all_without_10_13[!grepl("post", input_data_chop_all_without_10_13$filename),]
-# 
-# #write the input_data_chop to a new file
-# data_path <- "C:/Research_Assistant/work/data/TCR_data/"
-# write.csv(input_data_chop_all_reg_days_no_filename,paste0(data_path,'input_data_chop_all_reg_days_no_filename.csv'), row.names = FALSE)
-# 
-# myvars <- c("junction_aa", "duplicate_count", "day", "subject", "chain", "PCR_positive")
-# input_data_chop_all_reg_days_no_filename <- input_data_chop_all_without_reg_days[myvars]
-# 
-# input_data_chop_all_wide <- reshape(input_data_chop_all, idvar=c('junction_aa','subject','chain','PCR_positive'),timevar='day',direction='wide')
+#chop out all the patients who are PCR-ve, as we just want PCR+ve people in this case
+input_data_chop_all_PCR_pos <- input_data_chop_all[!grepl(0, input_data_chop_all$PCR_positive),]
 
 #chop out all the data from day 14 from the data set, as this will give us too many counts which are (0,0)
-input_data_chop_all_without_14 <- input_data_chop_all[!grepl(14, input_data_chop_all$day),]
+input_data_chop_all_without_14 <- input_data_chop_all_PCR_pos[!grepl(14, input_data_chop_all_PCR_pos$day),]
 
 #make a list containing all of the possible subjects for subsetting the data
 list_of_subjects <- as.list(unique(input_data_chop_all_without_14$subject))
 
-#filter out individual patients
+#filter out individual patients and make individual wide dataframes for them. Then bind all these dataframes together
 all_pats_wide <- data.frame()
-#for (n in 1:length(list_of_subjects)) {
-for (n in 1:2) {
-  a_subject <- input_data_chop_all_without_14 %>% filter(subject == list_of_subjects[n])
-  print(paste("patient",list_of_subjects[n]))
-  #next make the data frame wide
+for (n in 1:length(list_of_subjects)) {  #from 1 to the number of patients
+  a_subject <- input_data_chop_all_without_14 %>% filter(subject == list_of_subjects[n])  #subsetting the full dataframe for just 1 subject
+  #make the wide dataframe. Keeping 'junction_aa','subject','chain','PCR_positive'. And making 'duplicate_count.x', where x are the days in question (0 and 7 to begin with)
   a_subject_wide <- reshape(a_subject, idvar=c('junction_aa','subject','chain','PCR_positive'),timevar='day',direction='wide')
-  print(paste("number of rows of a_subject_wide",nrow(a_subject_wide)))
-  #bind this dataframe onto the dataframes for the other subjects
+  #bind the dataframe for this subject onto the dataframes for the other subjects
   all_pats_wide <- bind_rows(all_pats_wide,a_subject_wide)
-  print(paste("number of rows of all_pats_wide",nrow(all_pats_wide)))
 }
 
 
