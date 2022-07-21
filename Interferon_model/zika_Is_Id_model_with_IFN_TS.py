743f0859a13ba41f2b818084594d30db4613ea5c
David Scobie <david.scobie.20@ucl.ac.uk>
2022-07-21 11:39:38 +0100

    adding Id Is model 3 compartments with IFN to repo

--- Interferon_model/zika_Is_Id_model_with_IFN_TS.py
+++ Interferon_model/zika_Is_Id_model_with_IFN_TS.py
@@ -4,6 +4,11 @@ import numpy as np
 import seaborn
 from lmfit import minimize, Parameters, Parameter, report_fit
 from scipy.integrate import odeint
+from scipy.stats import norm, lognorm
+import matplotlib.mlab as mlab
+import scipy.stats as stats
+import statistics
+from matplotlib import cm
 
 #import the excel data
 
@@ -152,41 +157,18 @@ for j in effective_day:
         if i==j:
             div_vir_list_sum[int(2*(i-min(eff_day_vals)))]+=div_vir_list[int(k)]
             k+=1
-#print('div_vir_list_sum',div_vir_list_sum)
-"""
-plt.plot(eff_day_vals,div_vir_list_sum,'-rx')
-"""
 
-#how many patients do we have? do the patients get sick or stay healthy or both? (out of the 36)
 
 #find all the possible subject IDs
 Subject_ID = df2_str_sorted['Subject ID'].tolist()
 Subject_ID_vals = list(set(Subject_ID))
-#print('Subject_ID',Subject_ID)
+
 
 #append effective_day to the dataframe
 df2_str_sorted['effective_day'] = effective_day
 
 #plot the subjects in different colours
 df2_str_sorted['Subject ID'] = df2_str_sorted['Subject ID'].astype(str)
-
-#seaborn.relplot(data=df2_str_sorted, x='effective_day', y='Virus Titre (Log10 copies/mL)', hue='Subject ID')
-
-# plt.figure()
-# #seaborn.pointplot(data=df2_str_sorted, x='effective_day', y='Virus Titre (Log10 copies/mL)', hue='Subject ID', ci=None)
-
-# #plot individual patients on different days
-# Subject_ID_vals_short = Subject_ID_vals[0:3]   #just plotting the first patient as a check up
-# for j in Subject_ID_vals_short:
-#     k+=1
-#     #plt.figure()
-#     df2_Subj_ID_sorted = df2_str_sorted[df2_str_sorted['Subject ID'].str.contains(str(j)) == True]  #make a subset of the dataframe based on the subject ID
-#     df2_Subj_ID_sub_eff_sort = df2_Subj_ID_sorted.sort_values(["effective_day"], ascending=True) #sort the values of the dataframe based on the effective_day
-#     df2_Subj_ID_sub_eff_sort.plot(x='effective_day', y='Virus Titre (Log10 copies/mL)',kind='line',xlim=[1,18.5],ylim=[2.8,10.4]) #plot the subject points as a line plot
-
-#     plt.title('Subject ID=%i' %j)
-#     plt.xlabel('Study Day')
-#     plt.ylabel('Virus Titre (Log10 copies/mL)')
 
 
 #plot actual virus amount (instead of log10 of virus amount)
@@ -280,39 +262,25 @@ max_indic = int(max_indic_arr[0])
 
 a, b = best_fit(eff_day_vals[:max_indic+1],np.log10(act_div_vir_list_sum)[:max_indic+1])
 
-# plt.figure()
-# plt.scatter(eff_day_vals[:max_indic+1], np.log10(act_div_vir_list_sum)[:max_indic+1], marker='o', color='red', label='measured V data', s=75)
-# yfit = [a + b * xi for xi in eff_day_vals[:max_indic+1]]
-# print('yfit',yfit)
-# plt.plot(eff_day_vals[:max_indic+1], yfit)
-# plt.xlabel('Days Post Infection')
-# plt.ylabel('Virus Titre (Log10 copies/mL)')
-# plt.xlim(left=0)
-# plt.ylim(bottom=0)
-
 #add the point at time=0, virus=extrap val to the eff_day_vals and act_div_vir_list_sum arrays
 v1 = 0
 v2 = 10**a #a is the y intercept of the line of best fit
 eff_day_vals = np.insert(eff_day_vals, 0, v1, axis=0)
 act_div_vir_list_sum = np.insert(act_div_vir_list_sum, 0, v2, axis=0)
 print('eff_day_vals',eff_day_vals,'act_div_vir_list_sum',act_div_vir_list_sum)
-"""
-#paper initial conditions
-U0 = 4*(10**(8))  #the number of cells in an adult is 4x10^8
-V0 = 0.31   #cannot be measured as it is below detectable levels. Previous work has shown use <50 copies/ml
-I0 = 0   #Should be zero
-"""
+
 #my optimised initial conditions
 U0 = 4*(10**(8))  #the number of cells in an adult is 4x10^8
-#Is0 = act_div_vir_list_sum[0] / 2
 Is0 = act_div_vir_list_sum[0]
-#Id0 = act_div_vir_list_sum[0] / 2  #just taking the first measured value
+Is0_init = act_div_vir_list_sum[0] #for later on when I need to refer back to initial
 Id0 = 0
 y0 = [U0, Is0, Id0]
+y0_init = [U0, Is0, Id0] #for later on when I need to refer back to initial
 
-#assign t and V
 t_measured = eff_day_vals
+t_measured_init = t_measured #this is the timepoints for the average of the data across all patients
 V_measured = act_div_vir_list_sum
+V_measured_init = V_measured #this is the virus for the average of the data across all patients
 
 #plt.figure()
 fig, (ax1, ax2, ax3) = plt.subplots(1,3)
@@ -323,13 +291,7 @@ params = Parameters()
 params.add('U0', value=U0, vary=False)
 params.add('Is0', value=Is0, vary=False)
 params.add('Id0', value=Id0, vary=False)
-"""
-#parameters optimised on first 6 days of data
-params.add('alpha', value=4.24*(10**(-7)), min=4.23*(10**(-7)), max=4.25*(10**(-7)))   #rate that viral particles infect susceptible cells
-params.add('beta', value=61.2, min=61.1, max=61.3)    #Clearance rate of infected cells
-params.add('gamma', value=1.83, min=1.82, max=1.84)        #Infected cells release virus at rate gamma
-params.add('delta', value=1.45, min=1.44, max=1.46)     #clearance rate of virus particles
-"""
+
 #my optimised parameters - optimised with low kappa
 params.add('alpha', value=5.9*(10**(-8)), min=2.9*(10**(-9)), max=3.1*(10**(-6)))   #rate that viral particles infect susceptible cells
 params.add('beta', value=1*(10**(-11)), min=0, max=1.1*(10**(-11)))    #Clearance rate of infected cells
@@ -421,240 +383,7 @@ ax3.legend()
 ax3.set_xlabel('Days Post Infection')
 ax3.set_ylabel('Concentration (million copies/mL)')
 ax3.set_title('c)')
-"""
-#print('Is_fitted',Is_fitted,'Id_fitted',Id_fitted)
 
-#########################################################
-
-#############fit models to different patients
-
-#just start with trying to plot the first 2 subjects (to minimise the number of figures made)
-Subject_ID_vals_short = Subject_ID_vals[0:3]
-print('Subject_ID_vals_short',Subject_ID_vals_short)
-
-#initialise arrays of patient parameters
-alphas=[]
-betas=[]
-gammas=[]
-deltas=[]
-kappas = []
-red_chi_squs = []
-residuals = []
-sum_residuals_squs = []
-chi_squs = []
-ndatas = []
-variances = []
-subj_IDs_over_5=[]
-
-for j in Subject_ID_vals:
-
-    df2_Subj_ID_sorted = df2_str_sorted[df2_str_sorted['Subject ID'].str.contains(str(j)) == True]  #make a subset of the dataframe based on the subject ID
-    df2_Subj_ID_sub_eff_sort = df2_Subj_ID_sorted.sort_values(["effective_day"], ascending=True) #sort the values of the dataframe based on the effective_day
-
-    #only use the subjects with more than 5 data points
-    if len(df2_Subj_ID_sub_eff_sort['Virus Titre (Log10 copies/mL)'].tolist()) > 5 and j != 676586 and j != 635331:  #excluding the challenging patient
-    #if len(df2_Subj_ID_sub_eff_sort['Virus Titre (Log10 copies/mL)'].tolist()) > 5:
-        k+=1
-        #convert the virus and the effective day values to a list
-        div_vir_list_sum = df2_Subj_ID_sub_eff_sort['Virus Titre (Log10 copies/mL)'].tolist()
-        eff_day_list = df2_Subj_ID_sub_eff_sort['effective_day'].tolist()
-
-        #print('Virus',len(df2_Subj_ID_sub_eff_sort['Virus Titre (Log10 copies/mL)'].tolist()))   #print how many datapoints there are
-
-        print('SUBJECT ID ',j)
-
-        #compute the actual virus amount (not the log)
-        act_div_vir_list_sum = np.zeros(len(div_vir_list_sum))
-        for i in range (len(div_vir_list_sum)):
-            act_div_vir_list_sum[i] = 10**(div_vir_list_sum[i])
-
-        #print('initial V value',act_div_vir_list_sum[0])
-
-        #extrapolation to find first data point
-        # Get the indices of maximum element in eff_day_vals
-        max_indic_arr = np.where(act_div_vir_list_sum == np.amax(act_div_vir_list_sum))
-        max_indic = int(max_indic_arr[0])
-
-        a, b = best_fit(eff_day_list[:max_indic+1],np.log10(act_div_vir_list_sum)[:max_indic+1])
-
-        #add the point at time=0, virus=933 to the eff_day_vals and act_div_vir_list_sum arrays
-        v1 = 0
-        v2 = 10**a #THIS IS 10**a. where a is the y intercept of the line of best fit
-        eff_day_list = np.insert(eff_day_list, 0, v1, axis=0)
-        act_div_vir_list_sum = np.insert(act_div_vir_list_sum, 0, v2, axis=0)
-
-        #my optimised initial conditions
-        U0 = 4*(10**(8))  #the number of cells in an adult is 4x10^8
-        #Is0 = act_div_vir_list_sum[0] / 2
-        Is0 = act_div_vir_list_sum[0]
-        #Id0 = act_div_vir_list_sum[0] / 2  #just taking the first measured value
-        Id0 = 0
-        y0 = [U0, Is0, Id0]
-
-        # measured data
-        t_measured = eff_day_list
-        V_measured = act_div_vir_list_sum
-
-        # plt.figure()
-        # plt.scatter(t_measured, V_measured, marker='o', color='red', label='measured V data', s=75)
-
-        # set parameters including bounds; you can also fix parameters (use vary=False)
-        params = Parameters()
-        params.add('U0', value=U0, vary=False)
-        params.add('Id0', value=Id0, vary=False)
-        params.add('Is0', value=Is0, vary=False)
-
-        #my optimised parameters - optimised with low kappa
-        params.add('alpha', value=5.9*(10**(-8)), min=2.9*(10**(-9)), max=3.1*(10**(-6)))   #rate that viral particles infect susceptible cells
-        params.add('beta', value=1*(10**(-11)), min=0, max=1.1*(10**(-11)))    #Clearance rate of infected cells
-        params.add('gamma', value=18, min=0, max=200)        #Infected cells release virus at rate gamma
-        params.add('delta', value=48, min=0, max=200)     #clearance rate of virus particles
-        params.add('kappa', value=2.1*(10**-7), min=1*(10**-9), max=3*(10**-4))     #clearance rate of virus particles
-
-        # #my optimised parameters - optimised with medium kappa
-        # params.add('alpha', value=1.1*(10**(-7)), min=2.9*(10**(-8)), max=3.1*(10**(-6)))   #rate that viral particles infect susceptible cells
-        # params.add('beta', value=1*(10**(-11)), min=0, max=1.1*(10**(-11)))    #Clearance rate of infected cells
-        # params.add('gamma', value=40, min=0, max=200)        #Infected cells release virus at rate gamma
-        # params.add('delta', value=4.9, min=0, max=200)     #clearance rate of virus particles
-        # params.add('kappa', value=1.5*(10**-8), min=1*(10**-9), max=3*(10**-6))     #clearance rate of virus particles
-
-        # #optimised with high kappa
-        # params.add('alpha', value=5.9*(10**(-8)), min=2.9*(10**(-8)), max=3.1*(10**(-6)))   #rate that viral particles infect susceptible cells
-        # params.add('beta', value=1*(10**(-11)), min=0, max=1.1*(10**(-11)))    #Clearance rate of infected cells
-        # params.add('gamma', value=18, min=0, max=200)        #Infected cells release virus at rate gamma
-        # params.add('delta', value=48, min=0, max=200)     #clearance rate of virus particles
-        # params.add('kappa', value=2.1*(10**-7), min=1*(10**-9), max=3*(10**-6))     #clearance rate of virus particles
-
-        # fit model
-        result = minimize(residual, params, args=(t_measured, V_measured), method='leastsq')  # leastsq nelder
-        # check results of the fit
-        data_fitted = g(t_measured, y0, result.params)
-
-        #plot absolute values
-        # plt.plot(t_measured, data_fitted[:, 1], '-', linewidth=2, color='red', label='fitted V data')
-        # plt.legend()
-        # plt.xlim([0, max(t_measured)])
-        # plt.ylim([0, 1.1 * max(V_measured)])
-        # plt.xlabel('Days Post Infection')
-        # plt.ylabel('Virus Titre (Log10 copies/mL)')
-        # plt.title('Subject ID=%i' %j)
-
-        # display fitted statistics and append parameters to lists
-        subj_IDs_over_5.append(j)
-        report_fit(result)
-        #print('result params',result.params)
-        for name, param in result.params.items():
-            #print(f'{name:7s} {param.value:11.5f} {param.stderr:11.5f}')
-            if name == 'alpha':
-                alphas.append(param.value)
-            if name == 'beta':
-                betas.append(param.value)
-            if name == 'gamma':
-                gammas.append(param.value)
-            if name == 'delta':
-                deltas.append(param.value)
-            if name == 'kappa':
-                kappas.append(param.value)
-
-        red_chi_squs.append(result.redchi)
-        residuals.append(result.residual)
-        sum_residuals_squs.append(sum(((result.residual)**2)))
-        chi_squs.append(result.chisqr)
-        ndatas.append(result.ndata)
-
-        #plot the fitted data and the model for log(virus) against day
-        log_V_measured = np.log10(V_measured)
-        log_Is_fitted = np.log10(data_fitted[:, 1])
-        log_Id_fitted = np.log10(data_fitted[:, 2])
-        log_Id_Is_fitted = np.log10(data_fitted[:, 1] + data_fitted[:, 2])
-
-        # plt.figure()
-        # plt.scatter(t_measured, log_V_measured, marker='o', color='red', label='measured V data', s=75)
-        # plt.plot(t_measured, log_Id_Is_fitted, '-', linewidth=2, color='red', label='fitted (Id + Is) data')
-        # plt.plot(t_measured, log_Is_fitted, '-', linewidth=2, color='blue', label='fitted Is data')
-        # plt.plot(t_measured, log_Id_fitted, '-', linewidth=2, color='green', label='fitted Id data')
-        # #plt.ylim(bottom=0.9 * min(log_V_measured))
-        # #plt.xlim(left=0)
-        # plt.legend()
-        # plt.xlabel('Days Post Infection')
-        # plt.ylabel('Concentration (Log10 copies/mL)')
-        # plt.title('Subject ID=%i' %j)
-
-        #append the 4 parameter values to arrays
-        #alphas.append(alpha)
-
-print('alphas',alphas)
-print('betas',betas)
-print('gammas',gammas)
-print('deltas',deltas)
-print('kappas',kappas)
-#print('residuals',residuals)
-print('sum_residuals_squs',sum_residuals_squs)
-print('ndatas',ndatas)
-variances = np.array(sum_residuals_squs) / np.array(ndatas)
-print('variances',variances)
-print('average variance',sum(variances)/len(variances))
-
-
-############################ plot the distributions of alpha, beta, gamma and delta
-plt.figure()
-plt.hist(alphas, density=False, bins=10,color = "skyblue")
-plt.ylabel('Number of patients')
-plt.xlabel('Alpha')
-plt.title('Histogram of alpha values across individual patients')
-
-#plot the overall alpha (across all the patients) over the top
-y, x, _ = plt.hist(alphas, density=False, bins=10,color = "skyblue")
-X = [overall_alpha, overall_alpha]
-Y = [0, y.max()]
-plt.plot(X,Y,color='red')
-
-plt.figure()
-plt.hist(betas, density=False, bins=10,color = "skyblue")
-plt.ylabel('Number of patients')
-plt.xlabel('Beta')
-plt.title('Histogram of beta values across individual patients')
-
-#plot the overall beta (across all the patients) over the top
-y, x, _ = plt.hist(betas, density=False, bins=10,color = "skyblue")
-X = [overall_beta, overall_beta]
-Y = [0, y.max()]
-plt.plot(X,Y,color='red')
-
-plt.figure()
-plt.hist(gammas, density=False, bins=10,color = "skyblue")
-plt.ylabel('Number of patients')
-plt.xlabel('Gamma')
-plt.title('Histogram of gamma values across individual patients')
-
-#plot the overall gamma (across all the patients) over the top
-y, x, _ = plt.hist(gammas, density=False, bins=10,color = "skyblue")
-X = [overall_gamma, overall_gamma]
-Y = [0, y.max()]
-plt.plot(X,Y,color='red')
-
-plt.figure()
-plt.hist(deltas, density=False, bins=10,color = "skyblue")
-plt.ylabel('Number of patients')
-plt.xlabel('Delta')
-plt.title('Histogram of delta values across individual patients')
-
-#plot the overall delta (across all the patients) over the top
-y, x, _ = plt.hist(deltas, density=False, bins=10,color = "skyblue")
-X = [overall_delta, overall_delta]
-Y = [0, y.max()]
-plt.plot(X,Y,color='red')
-
-plt.figure()
-plt.hist(kappas, density=False, bins=10,color = "skyblue")
-plt.ylabel('Number of patients')
-plt.xlabel('Kappa')
-plt.title('Histogram of kappa values across individual patients')
-
-#plot the overall kappa (across all the patients) over the top
-y, x, _ = plt.hist(kappas, density=False, bins=10,color = "skyblue")
-X = [overall_kappa, overall_kappa]
-Y = [0, y.max()]
-plt.plot(X,Y,color='red')
-"""
 plt.show()
+############################
+
